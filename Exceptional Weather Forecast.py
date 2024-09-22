9513f7de6a6522d8acb0a65eb313f9dfbab251ae
Curtis J Pierce <Curtisjpierce39@gmail.com>
2024-09-22 16:27:57 -0700

    add initial description

--- Exceptional Weather Forecast.py
+++ Exceptional Weather Forecast.py
@@ -1,17 +1,15 @@
-#Question 1 Task 1
-
 def temp_conversion():
     try:
-        user_input = (input("Please enter a temperature in Farenheit: "))
+        user_input = (input("Please enter a temperature in Farenheit: "))#Question 1 Task 1
         user_temp = float(user_input)
-        temp_in_celsius = (user_temp - 32) * 5 / 9
+        temp_in_celsius = (user_temp - 32) * 5 / 9 #Question 1 Task 2
         rounded_number = round(temp_in_celsius, 2)
     except ValueError as ve:
         if "string to float" in str(ve):
             print("That is not a number. Please enter valid number.")
     else:
-        print(f"{user_input} degrees Farenheit is {rounded_number} degrees Celsius!")
+        print(f"{user_input} degrees Farenheit is {rounded_number} degrees Celsius!")#Question 1 Task 3
     finally:
-        print("Thank you for using the weather forecast application!")
+        print("Thank you for using the weather forecast application!")#Question 1 Task 4
         
 temp_conversion()
